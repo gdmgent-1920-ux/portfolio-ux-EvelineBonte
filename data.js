202d06deca51466b90b79c469f549d89b1d524cf
Eveline Bonte <evelbont@student.artevedldehs.be>
2020-02-26 21:03:03 +0100

    Update More results

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/3797857c-3f27-4573-5ec4-37eccb716435-41a6/?fullscreen",
                 "target": "_blank"
             }
         ]
